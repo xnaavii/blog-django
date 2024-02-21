bde90967bceb2ad6e3db64d73e6f9688043a319e
Ivan <petrovic.valpovo@gmail.com>
2024-02-21 15:37:48 +0000

    Add meta class to the models

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,13 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 class Post(models.Model):
+    # Adding a meta class
+    class Meta():
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
@@ -17,6 +24,7 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
+
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments"
     )
@@ -26,3 +34,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta():
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
